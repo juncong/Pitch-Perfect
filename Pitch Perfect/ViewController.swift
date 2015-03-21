eee5672435a3f598abd4db97ba0a25f62748831c
Jonathan Lin <juncong@umich.edu>
2015-03-21 20:20:12 +0800

    Initial Commit

---
+++ Pitch Perfect/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pitch Perfect
+//
+//  Created by Jonathan Lin on 21/3/15.
+//  Copyright (c) 2015 Jonathan Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
